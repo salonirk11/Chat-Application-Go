7c4959a50bdf451d14a75ad7ff262f5ff1c181cd
salonirk11 <salonirk11@gmail.com>
2020-04-02 23:22:37 +0530

    Add method to make new room

--- main.go
+++ main.go
@@ -24,7 +24,10 @@ func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
+	r := newRoom()
 	http.Handle("/", &templateHandler{filename: "chat.html"})
+	http.Handle("/room", r)
+	go r.run()
 	// start the web server
 	if err := http.ListenAndServe(":8080", nil); err != nil {
 		log.Fatal("ListenAndServe:", err)
