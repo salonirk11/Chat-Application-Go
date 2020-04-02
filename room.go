13f12bd652e60482cb209e58fcba570cb5f526ac
salonirk11 <salonirk11@gmail.com>
2020-04-02 22:53:16 +0530

    Add room model

--- room.go
+++ room.go
@@ -1,7 +1,8 @@
 package main
 
 type room struct {
-	// forward is a channel that holds incoming messages
-	//that should be forwarded to the other clients.
 	forward chan []byte
+	join chan *client
+	leave   chan *client
+	clients map[*client]bool
 }
